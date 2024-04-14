c12c8b422e90d1ce1949518cfa567063a1300506
Matt Kramer <matthew123003@gmail.com>
2024-04-14 14:31:33 -0400

    Updated config to ES6

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,9 +7,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 // Import stylesheets
-require("./style.css");
+import './style.css';
 const form = document.querySelector('#defineform');
 form.onsubmit = (event) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b, _c;
