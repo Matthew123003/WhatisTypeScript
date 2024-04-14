582dd0f61c42ff3d439e234c217ab7e88a327587
Matt Kramer <matthew123003@gmail.com>
2024-04-14 12:27:18 -0400

    Old code, new code in notes, still compiles

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // Import stylesheets
 require("./style.css");
 const form = document.querySelector('#defineform');
-// https://www.sohamkamani.com/typescript/rest-http-api-call/
-function getDefineWord(word) {
-    // const headers: Headers = new Headers()
-    // headers.set('Content-Type', 'application/json')
-    // headers.set('Accept', 'application/json')
-    // headers.set('X-Custom-Header', 'Test')
-    // Create the request object, which will be a RequestInfo type. 
-    // Here, we will pass in the URL as well as the options object as parameters.
-    const request = new Request(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, {
-        method: 'GET',
-        // headers: headers
-    });
-    return fetch(request)
-        .then(res => res.json())
-        .then(res => {
-        return res;
-    });
-}
-form.onsubmit = () => {
+form.onsubmit = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
+    event.preventDefault();
     const formData = new FormData(form);
-    console.log(formData);
-    const text = formData.get('defineword');
-    console.log(text);
-    var word = getDefineWord(text);
-    // const test: HTMLFormElement = document.querySelector('#test') as HTMLFormElement;
-    // test.innerText = word;
-    return false; // prevent reload
-};
+    const word = formData.get('defineword');
+    try {
+        const response = yield fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = yield response.json();
+        const definition = ((_c = (_b = (_a = data[0]) === null || _a === void 0 ? void 0 : _a.meanings[0]) === null || _b === void 0 ? void 0 : _b.definitions[0]) === null || _c === void 0 ? void 0 : _c.definition) || 'Definition not found.';
+        const definitionElement = document.getElementById('definition');
+        if (definitionElement) {
+            definitionElement.innerText = definition;
+        }
+        else {
+            console.error('Element with id "definition" not found');
+        }
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
